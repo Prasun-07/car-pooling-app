182d5c6d0b28130e3c6bf59d8d84308a2f4d0f36
Prasun-07 <biswasprasun67@gmail.com>
2025-05-12 13:55:29 +0530

    Add initial files

---
+++ src/app/components/CarImage.tsx
@@ -0,0 +1,5 @@
+export default function CarImage(){
+    return(
+        <div></div>
+    )
+}
---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,13 @@
+export default function NavBar(){
+    return(
+        <div className="flex fle">
+            <h1>Car Pool</h1>
+            <ul className="flex flex-wrap">
+                <li>Home</li>
+                <li>About</li>
+                <li>Help</li>
+                <li>Profile</li>
+            </ul>
+        </div>
+    )
+}
